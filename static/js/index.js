9e2307e9463e7cd67606a390adfb6e430dc4cc75
Austin Kellar <ajk@eduroam-169-233-252-74.ucsc.edu>
2018-11-02 12:35:11 -0700

    final exercise finished

--- static/js/index.js
+++ static/js/index.js
@@ -9,9 +9,6 @@ var enumerate = function(arr) {
 
 var processMovies = function() {
     enumerate(app.movies);
-    app.movies.map(function(movie) {
-
-    });
 };
 
 var onPageLoad = function() {
@@ -19,7 +16,6 @@ var onPageLoad = function() {
         function(response) {
             app.movies = response.movies;
             processMovies();
-            console.log(app.movies);
         }
     );
 };
@@ -28,13 +24,13 @@ var insertMovie = function() {
     var newMovie = {
         title: app.newMovieTitle,
         description: app.newMovieDescription,
-        rating: app.newMovieRating,
-        thumb: null
+        rating: app.newMovieRating
     };
     $.post('/moviesWithVue3/api/insert_movie/', newMovie, function(response) { 
         // the server responded with the id number of the new movie in the database. make sure to add this to the
         // new movie object before we add it to the view
         newMovie['id'] = response.new_movie_id;
+        newMovie['thumb'] = null; // the new movie should not have a thumb value yet!
         app.movies.push(newMovie);
         processMovies(); // ned to re-index the movies now that a new one has been added to thea array
     });
@@ -50,7 +46,6 @@ var handleThumbClick = function(movieIdx, newThumbState) {
     $.post('/moviesWithVue3/api/set_thumb/', { id: app.movies[movieIdx].id, thumb_state: setThumbTo }, function(response) {
         // after the web2py server responds, we know the thumb as been updated in the database
         // now, we just have to display the new thumb on the screen
-        console.log(setThumbTo)
         app.movies[movieIdx].thumb = setThumbTo;
     });
 };
